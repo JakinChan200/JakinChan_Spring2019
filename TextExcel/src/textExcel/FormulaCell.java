8bd16cc0eeeebc118f894632563d24d3b789c92e
ggigy <ggigy@LAPTOP-5N1V4DJ4>
2019-03-26 01:38:14 -0700

    Finished checkpoint 4

--- TextExcel/src/textExcel/FormulaCell.java
+++ TextExcel/src/textExcel/FormulaCell.java
@@ -8,12 +8,33 @@ public class FormulaCell extends RealCell{
 		this.formula = formula;
 	}
 	
+	public double getDoubleValue(String equation){
+		String[] equationParts = equation.split(" ");
+		double finalValue = Double.parseDouble(equationParts[1]);
+		for(int i = 1; i < equationParts.length - 2; i+=2){
+			switch(equationParts[i+1]){
+				case "+":
+					finalValue += Double.parseDouble(equationParts[i+2]);
+					break;
+				case "-":
+					finalValue -= Double.parseDouble(equationParts[i+2]);
+					break;
+				case "*":
+					finalValue *= Double.parseDouble(equationParts[i+2]);
+					break;
+				default:
+					finalValue /= Double.parseDouble(equationParts[i+2]);
+			}
+		}
+		return finalValue;
+	}
+	
 	public String fullCellText() {
 		return formula;
 	}
 	
 	public String abbreviatedCellText() {
-		formula = formula + "          ";
-		return formula.substring(0, 10);
+		String formula2 = getDoubleValue(formula) + "          ";
+		return formula2.substring(0, 10);
 	}
 }
