2c687524d72724bd620c301b63ec082dc19ddecd
ggigy <ggigy@LAPTOP-5N1V4DJ4>
2019-03-29 01:29:42 -0700

    Finished checkpoint 5

--- TextExcel/src/textExcel/FormulaCell.java
+++ TextExcel/src/textExcel/FormulaCell.java
@@ -12,11 +12,26 @@ public class FormulaCell extends RealCell{
 	
 	public double getDoubleValue(String equation){
 		String[] equationParts = equation.split(" ");
-		if(equationParts[1].equals("SUM")) {
-			return 0.0;
-		}else if(equationParts[1].equals("AVG")) {
-			return 0.0;
-		}else {
+		String[] valueBoundaries = equationParts[2].split("-");
+		if(equation.contains("-") && (equationParts.length == 4)){
+			double sum = 0;
+			double counter = 0;
+			Location beginningLoc = new SpreadsheetLocation(valueBoundaries[0]);
+			Location endingLoc = new SpreadsheetLocation(valueBoundaries[1]);
+			for(int row = beginningLoc.getRow(); row <= endingLoc.getRow(); row++){
+				for(int col = beginningLoc.getCol(); col <= endingLoc.getCol(); col++){
+					sum += Double.parseDouble(sheet[row][col].abbreviatedCellText());
+					if(equationParts[1].toUpperCase().equals("AVG")){
+						counter++;
+					}
+				}
+			}
+			if(equationParts[1].toUpperCase().equals("AVG")){
+				return sum / counter;
+			}else{
+				return sum;
+			}
+		}else{
 			double finalValue = getValue(equationParts[1]);
 			for(int i = 1; i < equationParts.length - 2; i+=2){
 				finalValue = operation(finalValue, equationParts[i+1], equationParts[i+2]);
@@ -24,6 +39,7 @@ public class FormulaCell extends RealCell{
 			return finalValue;
 		}
 	}
+	
 	
 	public double operation(double firstValue, String operator, String secondValue) {
 		double finalValue = firstValue;
@@ -46,7 +62,7 @@ public class FormulaCell extends RealCell{
 	public double getValue(String value) {
 		if(value.charAt(0) >= 65) {
 			Location loc = new SpreadsheetLocation(value);
-			return Double.parseDouble(sheet[loc.getRow()][loc.getCol()].fullCellText());
+			return Double.parseDouble(sheet[loc.getRow()][loc.getCol()].abbreviatedCellText());
 		}else {
 			return Double.parseDouble(value);
 		}
